6bd250e7be82db9233ccf6e9b8077b2333f25844
Shisui212 <ezshisui2127@gmail.com>
2025-10-02 09:47:40 +0200

    commit

--- modules/employes.py
+++ modules/employes.py
@@ -6,6 +6,23 @@ class Employes:
         self.salaire = salary
         self.id = id 
         self.poste= job
+        
 
+
+
+
+
+
+class Paiement:
+    def __init__(self, montant, mode):
+        self.montant = montant            
+        self.mode = mode                  
+    
+    def afficher_details(self):
+        print(f"Montant payé : {self.montant} €")
+        print(f"Mode de paiement : {self.mode}")
+
+    def est_valide(self):
+        return self.montant > 0
         
     
