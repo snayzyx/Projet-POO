0a49771bbc324bdd93e0c93660b89504b4f2f829
skymord <ethan.lambertp@gmail.com>
2025-10-02 09:26:05 +0200

    Commit

--- modules/User.py
+++
@@ -1,9 +0,0 @@
-class user:
-    def __init__(self,name,pay,solde,instant):
-        self.pay = pay
-        self.solde = solde
-        self.instant = instant
-        self.name = name
-    
-    def payement (self , name , solde):
-        
--- modules/Webserver.py
+++
@@ -1,6 +0,0 @@
-class webserver:
-    def __init__(self,host,port):
-        self.port = port 
-        self.host = host
-
-        
