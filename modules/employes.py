9aada9df4b5459900d9fbad4c3d860be28170a2a
skymord <ethan.lambertp@gmail.com>
2025-10-02 09:58:12 +0200

    Commit

---
+++ modules/employes.py
@@ -0,0 +1,10 @@
+class Employes:
+
+    def __init__(self,lastname,firstname,salary,id,job):
+        self.prenom = firstname
+        self.nom = lastname
+        self.salaire = salary
+        self.id = id 
+        self.poste= job
+
+    
